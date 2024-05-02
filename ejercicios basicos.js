35b69d39a00342db3d3521ef8502bc7ed8d599c2
Kumi <Kumikoaisaka@gmail.com>
2024-05-02 19:50:29 +0200

    subido ejercicios de javascript

---
+++ ejercicios basicos.js
@@ -0,0 +1,28 @@
+
+/*
+Has hecho una compra y sabes el precio del productoy su iva. Haz un script que te calcule el precio total que vas a 
+pagar por tu compra.
+Te recuerdo que para calcular el total debes sumar al precio el resuelto de multiplicar precio por el iva y dividir por 100
+*/
+
+let precio = 200;
+
+let precioTotal = 200 + 21*200/100;
+
+
+console.log(precioTotal);
+
+/*
+En una variable tienes el lado de un cuadrado, debes escribir un programa que te 
+calcule el área y el perímetro del cuadrado.
+El area la calculas como lado multiplicado por lado. El perímetro es la suma de los cuatro lados.
+*/
+
+let lado = 40;
+let area = lado * lado;
+let perimetro = lado * 4 ;
+
+console.log(area);
+
+console.log(perimetro);
+
